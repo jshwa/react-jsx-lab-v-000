8d1f8a4f9f3032f440565282013e5929290feaac
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-31 22:09:07 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -4,9 +4,11 @@ import FillerText from './FillerText'
 class Webpage extends React.Component{
   render(){
     return (
-      <h1>The World's coolest webpage</h1>
-      <FillerText />
-      <FillerText />
+      <div>
+        <title>The World's coolest webpage</title>
+        <FillerText />
+        <FillerText />
+      </div>
     )
   }
 }
