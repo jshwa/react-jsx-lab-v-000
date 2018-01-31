69b8d5c8d0fce4654b12a1288d53760bde978ee9
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-31 21:50:22 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,1 +1,10 @@
-class 
+class RegistrationForm extends React.component{
+  render(){
+    <form>
+      <input type="text" />
+      <input type="password" />
+    </form>
+  }
+}
+
+export RegistrationForm
